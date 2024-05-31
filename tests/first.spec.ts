e2d4da0053823fb08b1c602224c5a601e664f6ec
Nav-2d <navdeep.singh2088@gmail.com>
2024-05-31 13:39:14 -0700

    lets go

---
+++ tests/first.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test("first test in the first file", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await page.getByRole("link", { name: "Get started" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
